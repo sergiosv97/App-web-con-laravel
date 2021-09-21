efec708a93c7ed682e0d5f7aa9acbb9ea100fd5c
sergio <sergiosanchezv20@gmail.com>
2021-09-21 12:13:58 -0400

    edit funcional y refactorizacion de vistas blade

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
+@extends('layouts.main')
+@section('contenido')
+
     <div class="container"><br>
         <div class="row">
             <div class="col-md-12">
@@ -16,7 +9,8 @@
                         Editar producto
                     </div> 
                     <div class="card-body">
-                        <form action="" method="POST">
+                        <form action="{{route('products.update', $product->id)}}" method="POST">
+                            @method('put')
                             @csrf
                             <div class="form-group">
                                 <label for="">Descripcion</label>
@@ -34,5 +28,4 @@
             </div>
         </div>   
     </div>
-</body>
-</html>
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
+@extends('layouts.main')
+@section('contenido')
+    
     <div class="container"><br>
         <div class="row">
             <div class="col-md-12">
@@ -54,5 +47,4 @@
             </div>
         </div>   
     </div>
-</body>
-</html>
+@endsection
